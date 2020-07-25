e5f1e1999e9152718a7f8fc27d53f0c020369d45
Baum <64490493+BAUMomri@users.noreply.github.com>
2020-07-25 19:19:31 +0300

    FINAL

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict'
 var gameOn = false;
-var activeHint = true;
+var activeHint = false;
 var gameOver = false;
 var gameWon = false;
 var firstClick = true;
@@ -9,22 +9,23 @@ var gLevel;
 var gGame;
 var curTimer;
 var curLevel = 'easy';
+var hintCnt = 0;
 var levels = {
-    easy:{
-        MINES:2,
-        size:4
-    }, 
-
-    medium:{
-        MINES:4,
-        size:8
+    easy: {
+        mines: 2,
+        size: 4
     },
-    hard:{
-        MINES:8,
-        size:12
+
+    medium: {
+        mines: 16,
+        size: 8
+    },
+    hard: {
+        mines: 36,
+        size: 12
     }
 };
-var hintCnt = 0;
+
 
 
 function initGame() {
@@ -32,13 +33,10 @@ function initGame() {
     renderBoard(gBoard);
 }
 
-// function setActiveLevel(nameLevel){
-//     activeLevel = levels[nameLevel];
-// }
 
 function buildBoard() {
     var board = [];
-    /// i =>levels[activeLevel].size
+
     for (var i = 0; i < levels[curLevel].size; i++) {
         board[i] = [];
         for (var j = 0; j < levels[curLevel].size; j++) {
@@ -55,16 +53,29 @@ function buildBoard() {
 
     return board;
 }
-function resetGame(){
+
+function resetGame() {
+    gameOn = false
+    activeHint = true;
+    gameOver = false;
+    gameWon = false;
+    firstClick = true;
+    gBoard;
+    gLevel;
+    gGame;
+    curTimer;
+    curLevel = 'easy';
     clearInterval(curTimer);
-    gameOn = false;
     document.querySelector('.task-bar-buttons').classList.remove('hide');
     document.querySelector('#reset').classList.add('hide');
     document.querySelector('#hint').classList.add('hide');
     document.getElementById("minutes").innerText = '00';
     document.getElementById("seconds").innerText = '00';
     initGame();
+
 }
+
+
 function renderBoard(board) {
     var strHTML = '';
     for (var i = 0; i < board.length; i++) {
@@ -84,29 +95,32 @@ function renderBoard(board) {
 
     document.querySelector('.task-bar-buttons').innerHTML = '<button class="game" onclick="EasyLevel()">Easy</button> <button class="game" onclick="MediumLevel()">Medium</button> <button class="game" onclick="HardLevel()">Hard</button>';
     var taskBar = document.querySelector('.task-bar');
-    if(!document.querySelector('#reset'))
-        taskBar.innerHTML = `<button id="reset" class="hide" onClick="resetGame();" data-state="idle" ></button>` +taskBar.innerHTML;
-    if(!document.querySelector('#hint'))
-        taskBar.innerHTML = `<button id="hint" class="hide" onClick="toggleHint();" data-hint="off" ></button>` +taskBar.innerHTML;
+    if (!document.querySelector('#reset'))
+        taskBar.innerHTML = `<button id="reset" class="hide" onClick="resetGame();" data-state="idle" ></button>` + taskBar.innerHTML;
+    if (!document.querySelector('#hint'))
+        taskBar.innerHTML = `<button id="hint" class="hide" onClick="toggleHint();" data-hint="off" ><span class="cnt">3</span></button>` + taskBar.innerHTML;
 }
-function toggleHint(){
-    var hint  = document.querySelector('#hint');
-    hint.dataset.hint = hint.dataset.hint == 'off'? 'on' : 'off';
+
+function toggleHint() {
+    if (!gameOver && hintCnt<3 ) {
+        var hint = document.querySelector('#hint');
+        hint.dataset.hint = hint.dataset.hint == 'off' ? 'on' : 'off';
+    }
 }
-function EasyLevel(){
-console.log('EasyLevel')
-curLevel = 'easy';
-initGame();
+
+function EasyLevel(e) {
+    curLevel = 'easy';
+    initGame();
 }
-function MediumLevel(){
-console.log('MediumLevel')
-curLevel = 'medium';
-initGame();
+
+function MediumLevel() {
+    curLevel = 'medium';
+    initGame();
 }
-function HardLevel(){
-console.log('HardLevel')
-curLevel = 'hard';
-initGame();
+
+function HardLevel() {
+    curLevel = 'hard';
+    initGame();
 }
 
 function timeCounter() {
@@ -132,72 +146,74 @@ function timeCounter() {
     }
 
 }
-function setResetBtn(){
+
+function setResetBtn() {
     document.querySelector('.task-bar-buttons').classList.add('hide');
     document.querySelector('#reset').classList.remove('hide');
     document.querySelector('#hint').classList.remove('hide');
-    
-
 }
+
 function cellClicked(minesAroundCount, i, j) {
 
     var selectedCell = gBoard[i][j];
     if (gameOver) return;
     if (selectedCell.isMarked === true) return;
     if (selectedCell.isShown === true) return;
-    if (firstClick === true) {
-        createMines(i, j)
-        firstClick = false;
-    }
     if (gameOn === false) {
         gameOn = true;
+        createMines(i, j)
         setMinesNegsCount(gBoard)
-        openNeighbors(i, j, gBoard)
+        openNeighbors(i, j, gBoard,true)
         setResetBtn()
         timeCounter();
     } else {
-        if(hintCnt < 3 && document.querySelector('#hint').dataset.hint == 'on'){
+        if (hintCnt < 3 && !gameOver && document.querySelector('#hint').dataset.hint == 'on') {
             hintCnt++;
             var cur = event.currentTarget;
             cur.classList.add('show')
             cur.innerHTML = gBoard[i][j].minesAroundCount
-            setTimeout(function(){
-                cur.innerHTML = '';
-                cur.classList.remove('show')
-            },1000);
+            openNeighbors(i, j, gBoard,true)
+           
+            document.querySelector('#hint span').innerText = 3- hintCnt;
+            setTimeout(function () {
+                openNeighbors(i, j, gBoard, false)
+                document.querySelector('#hint').dataset.hint = 'off'
+            }, 1000);
+            
             return;
         }
-         if (selectedCell.isMine === true) {
+        if (selectedCell.isMine === true) {
             selectedCell.isShown = true;
             addBombImg(i, j);
             clearInterval(curTimer);
-            document.querySelector('.task-bar').dataset.state ='lose';
+            document.querySelector('#reset').dataset.state = 'lose';
             gameOver = true;
+            setTimeout(function () {
+                alert('THE GAME IS OVER! You stepped on a mine... \nclick on the soldier to play again!')
+            }, 100);
             return;
         }
         if (minesAroundCount == 0) {
-            openNeighbors(i, j, gBoard);
+            openNeighbors(i, j, gBoard,true);
         }
     }
 
     selectedCell.isShown = true;
     checkGameOver(i, j);
-
 }
 
-function addBombImg(i, j) {
+function addBombImg(i, j ,isRemove) {
     var cellId = `cell-${i}-${j}`;
     var cellEl = document.getElementById(cellId)
     cellEl.classList.add('bomb');
-    cellEl.innerHTML = '<img src="./img/mine.png" width="20" height="20"></img>';
+    cellEl.innerHTML = isRemove? '':'<img src="./img/mine.png" width="28" height="28"></img>';
 }
 
 function createMines(i, j) {
     var bombCount = 0;
-    // 2 => levels[activeLevel].MINES
-    while (bombCount < levels[curLevel].MINES) {
-        var randomI = Math.floor(Math.random() * 3) + 1;
-        var randomJ = Math.floor(Math.random() * 3) + 1;
+    while (bombCount < levels[curLevel].mines) {
+        var randomI = Math.floor(Math.random() * levels[curLevel].size);
+        var randomJ = Math.floor(Math.random() * levels[curLevel].size);
         if ((randomI == i && randomJ == j) ||
             (randomI == i - 1 && randomJ == j - 1) ||
             (randomI == i - 1 && randomJ == j) ||
@@ -229,33 +245,59 @@ function countNeighbors(cellI, cellJ, board) {
     return neighborsSum;
 }
 
-function openNeighbors(cellI, cellJ, board) {
+function openNeighbors(cellI, cellJ, board ,isShow) {
     var clickedCellId = `cell-${cellI}-${cellJ}`;
     var clickedCellEl = document.getElementById(clickedCellId);
-    clickedCellEl.classList.add('clicked-cell')
-    clickedCellEl.innerHTML = '0';
+    var boardCell = board[cellI][cellJ];
+    console.log(boardCell.isMine ,  hint.dataset.hint,  false )
+
+    isShow?
+        clickedCellEl.classList.add('clicked-cell') :
+        clickedCellEl.classList.remove('clicked-cell') 
+    // clickedCellEl.innerHTML = isShow? 'F' : '';
+
+    if(boardCell.isMine &&  hint.dataset.hint == "on" && isShow){
+        addBombImg(cellI, cellJ);
+        return;
+    } else if  (boardCell.isMine &&  hint.dataset.hint == "on" && isShow == false){
+        console.log('run remove after if')
+        addBombImg(cellI, cellJ, true);
+        return;
+    }
     for (var i = cellI - 1; i <= cellI + 1; i++) {
         if (i < 0 || i >= board.length) continue;
         for (var j = cellJ - 1; j <= cellJ + 1; j++) {
             if (board[i][j] != undefined) {
                 if (i === cellI && j === cellJ) continue;
                 if (j < 0 || j >= board[i].length) continue;
-                if (board[i][j].isMarked === true || board[i][j].isMine === true) continue;
+                if (board[i][j].isMarked === true || board[i][j].isMine === true){
+                  
+                    continue;
+                } 
 
-                board[i][j].isShown = true;
+                board[i][j].isShown = isShow;
                 var cellId = `cell-${i}-${j}`;
                 var cellEl = document.getElementById(cellId);
-                cellEl.classList.add('show');
-                cellEl.classList.add('clicked-cell')
-                cellEl.innerHTML = board[i][j].minesAroundCount;
-
+                if(isShow){
+                    cellEl.classList.add('show');
+                    cellEl.classList.add('clicked-cell')
+                
+                }else{
+                   
+                    cellEl.classList.remove('clicked-cell')
+                    cellEl.classList.remove('show')
+                }
+                    
+               
+                // if (board[i][j].minesAroundCount != 0){
+                cellEl.innerHTML = isShow ?board[i][j].minesAroundCount ==0? '':board[i][j].minesAroundCount :'';
+                
             }
         }
     }
 
 }
 
-// Count mines around each cell and set the cell's minesAroundCount.
 function setMinesNegsCount(board) {
     for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
@@ -269,6 +311,7 @@ function setMinesNegsCount(board) {
 
 function cellMarked(i, j) {
     if (gameOver) return;
+    setResetBtn()
     var cellId = `cell-${i}-${j}`;
     var cellEl = document.getElementById(cellId);
     if (gBoard[i][j].isMarked === true && gameOn) {
@@ -278,13 +321,20 @@ function cellMarked(i, j) {
     } else {
         if (!gBoard[i][j].isShown) {
             gBoard[i][j].isMarked = true;
-            if (!gameOn) timeCounter();
-            cellEl.innerHTML = "<img src=\"./img/flag.png\" width=\"20\" height=\"20\"></img>";
+            if (!gameOn) {
+                gameOn = true;
+                createMines(i, j)
+                setMinesNegsCount(gBoard)
+                setResetBtn()
+                timeCounter();
+                clearInterval(curTimer);
+                }
+            cellEl.classList.add('show');
+            cellEl.innerHTML = '<img src="./img/flag.png" width="28" height="28"></img>';
+            checkGameOver(i, j);
         }
     }
 }
-
-// Game ends when all mines are marked, and all the other cells are shown
 
 function countNeighbors(cellI, cellJ, board) {
     var neighborsSum = 0;
@@ -303,16 +353,20 @@ function countNeighbors(cellI, cellJ, board) {
 
 
 function checkGameOver() {
-    for (var i = 0; i < gBoard.length; i++){
-            for (var j = 0; j < gBoard[0].length; j++)
-                if (gBoard[i][j].isMarked || gBoard[i][j].isShown) {
-                    continue;
-                } else {
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[0].length; j++)
+            if (gBoard[i][j].isMarked && gBoard[i][j].isMine || gBoard[i][j].isShown) {
+                continue;
+            } else {
+                return;
+            }
+    }
 
-                    return;
-                }
-        }
-        document.querySelector('.task-bar').dataset.state = 'win'
-        alert('You won!!')
-        console.log('WON!!!')
-    } 
+    if (!activeHint){
+    setTimeout(function () {
+        clearInterval(curTimer);
+        document.querySelector('#reset').dataset.state = 'win'
+        alert('YOU WIN! \nclick on the soldier to play again!')
+    }, 100);
+}
+}
